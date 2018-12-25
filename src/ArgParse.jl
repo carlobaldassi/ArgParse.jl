c940640dac3f153ae1df048335827ec7997de8e7
Robert Hönig <robhoenig@gmail.com>
2018-12-25 13:56:55 +0100

    Use @warn instead of warn().
    warn() has been removed in Julia 1.0.

--- src/ArgParse.jl
+++ src/ArgParse.jl
@@ -368,7 +368,7 @@ end
 function warn_extra_opts(opts, valid_keys::Vector{Symbol})
 
     for k in opts
-        k in valid_keys || warn("ignored option: $k")
+        k in valid_keys || @warn "ignored option: $k"
     end
     return true
 end
@@ -801,7 +801,7 @@ macro add_arg_table(s, x...)
             continue
         else
             # anything else: ignore, but issue a warning
-            warn("@add_arg_table: ignoring expression $y")
+            @warn "@add_arg_table: ignoring expression $y"
             i += 1
         end
     end
@@ -1489,7 +1489,7 @@ function check_settings_can_use_symbols(settings::ArgParseSettings)
     end
     settings.suppress_warnings && return true
     for f in args_table.fields
-        '-' in f.dest_name && warn("dest_name=$(f.dest_name) contains an hyphen; use the autofix_names=true setting to have it converted to an underscore")
+        '-' in f.dest_name && @warn "dest_name=$(f.dest_name) contains an hyphen; use the autofix_names=true setting to have it converted to an underscore"
     end
     return true
 end
